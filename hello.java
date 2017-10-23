fcd28766a902ee9dfa76afecadb6df800ec384a3
Sherardy <sherardy@users.noreply.github.com>
2017-10-23 16:58:48 -0400

    added useless user input

--- hello.java
+++ hello.java
@@ -1,16 +1,12 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
-
-
-	// added  line	
-
-        System.out.println("Hello Class!");
-
-
-
+      Scanner in=new Scanner(System.in);
+      System.out.println("Input something:");
+      String input=in.nextLine();
+      System.out.println(input);
+        
     }
 }
-
